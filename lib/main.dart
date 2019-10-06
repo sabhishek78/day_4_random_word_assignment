c788c79ffb64bce539f957ed4aaacfcee069a6e7
abhishek shrivastava <abhishekshrivastava78@gmail.com>
2019-10-06 19:08:28 +0530

    password generator finished package used to generate random words Output converted to camel case

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'words.dart';
 
 import 'package:day_4_random_word_assignment/words.dart';
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:recase/recase.dart';
+
 
 void main() {
   runApp(
@@ -27,16 +30,23 @@ class _PasswordGeneratorState extends State<PasswordGenerator> {
 
 
   String generatePassword() {
+   String word;
 
-    int n = words.length;
-    int index;
-    Random rand = Random();
     setState(() {
-      index=rand.nextInt(n - 1);
+     word=WordPair.random().first;
     });
-    return words[index];
+   // print(word);
+    return word;
   }
 
+   String convertCamelCase(String s1, String s2, String s3)
+   {
+     String join=s1+'_'+s2+'_'+s3;
+     print(join);
+     ReCase rc = new ReCase(join);
+     join=rc.camelCase;
+     return join;
+   }
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +55,7 @@ class _PasswordGeneratorState extends State<PasswordGenerator> {
       word1 = generatePassword();
       word2 = generatePassword();
       word3 = generatePassword();
-
+      //convertCamelCase(word1, word2, word3);
 
     return Scaffold(
         backgroundColor: Colors.white,
@@ -58,7 +68,7 @@ class _PasswordGeneratorState extends State<PasswordGenerator> {
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text('$word1 $word2 $word3',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
+                Text( convertCamelCase(word1, word2, word3),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
                 SizedBox(height: 20,),
                 FlatButton(
                   color: Colors.blue,
